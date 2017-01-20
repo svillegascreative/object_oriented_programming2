e9157c6fd868c146aabd9b43083dddac80b5f9a8
sylvia villegas <sylvia@svillegas.com>
2017-01-19 19:40:19 -0500

    Compelte exercise 1

--- exercise1-people.rb
+++ exercise1-people.rb
@@ -1,30 +1,33 @@
-class People
+class Person
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
+  def greet
+    puts "Hi, my name is #{@name}"
+  end
 end
 
 class Student < Person
-
-  def initialize
-  end
-
   def learn
     puts "I get it!"
   end
-
 end
 
-
 class Instructor < Person
-
-  def initialize
-  end
-
   def teach
     puts "Everything in Ruby is an object!"
   end
-
 end
+
+
+chris = Instructor.new("Chris")
+chris.greet
+chris.teach
+
+cristina = Student.new("Cristina")
+cristina.greet
+cristina.learn
+cristina.teach # FAIL: teach method only available to Instructor class
